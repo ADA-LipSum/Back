6e7d1dcdba52f799c5e7e984e8f6b4f4183f26ad
Haryu5412 <haryu5412@gbsw.gyo6.net>
2026-04-16 19:13:31 +0900

    deploy: 테스트

--- src/main/java/com/ada/proj/ProjApplication.java
+++ src/main/java/com/ada/proj/ProjApplication.java
@@ -2,12 +2,13 @@ package com.ada.proj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+
+import com.ada.proj.config.AutoIncrementProperties;
 import com.ada.proj.config.CookieProperties;
 import com.ada.proj.config.CorsProperties;
 import com.ada.proj.config.JwtProperties;
-import com.ada.proj.config.AutoIncrementProperties;
 
 @SpringBootApplication
 @EnableCaching
@@ -16,6 +17,7 @@ public class ProjApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjApplication.class, args);
+        // idk
     }
 
 }
